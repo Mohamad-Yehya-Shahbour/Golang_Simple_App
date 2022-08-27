d5f43412219610aa3d79b101e84615adaf1b9b8d
Mohamad Yehya <mohamad.yehya.shahbur@gmail.com>
2022-08-27 23:46:55 +0300

    add makeConnection funtion

--- Application/db.go
+++ Application/db.go
@@ -7,3 +7,15 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
+
+func makeConnection()*gorm.DB{
+
+	dsn := "root:@tcp(127.0.0.1:3306)/golangdb?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		fmt.Println("Error while connecting the database", err.Error())
+	}
+
+	return db
+}
